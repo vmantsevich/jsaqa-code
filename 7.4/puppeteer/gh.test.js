3acbc6830e9c1f06f23994993d064802bc2b1d14
VMantsevich <vladislava_mantsevich@mail.ru>
2023-09-22 19:25:13 +0300

    Initial commit

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,8 +1,8 @@
 let page;
+const timeout = 3000;
 
 beforeEach(async () => {
-  page = await browser.newPage();
-  await page.goto("https://github.com/team");
+  page = await browser.newPage();    
 });
 
 afterEach(() => {
@@ -10,25 +10,55 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
+  beforeEach(async () => {    
+    await page.goto("https://github.com/team");    
+  }, timeout);
+
+  test("The h1 header content", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
   });
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", link => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
   });
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
-      visible: true,
+      visible: true      
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    
+    const actual = await page.$eval(btnSelector, link => link.textContent, { timeout: 4000 });
+    expect(actual).toContain("Get started with Team")
   });
+});
+
+test("GitHub pricing title content", async () => { 
+  await page.goto("https://github.com/pricing");
+
+  await page.waitForSelector('h1');    
+  const title = await page.title();
+  
+  expect(title).toEqual("Pricing · Plans for every developer · GitHub");
+});
+
+test("GutHub pricing header content", async () => {
+  await page.goto("https://github.com/pricing");
+  const h1 = await page.$eval("h1.h2-mktg", (header) => header.textContent);    
+  
+  expect(h1).toContain("Get the complete");
+});
+
+test("GitHub Join header content", async () => { 
+  await page.goto("https://github.com/pricing"); 
+  await page.click("[test_selector='plan-input-business']");  
+  await page.waitForTimeout(1000);
+  const h1 = await page.$eval("h1.h1", (header) => header.textContent);    
+  
+  expect(h1).toEqual("First, let's create your user account");   
 });
